a96a592f3190c643614f88bd666dc515af515b45
Arturo Rodríguez Herrera <arthurod91@users.noreply.github.com>
2019-06-22 23:36:54 -0500

    Comentarios del código de main.js
    Comentarios al código incluyendo sobre algunas cosas que faltan

--- js/main.js
+++ js/main.js
@@ -2,34 +2,38 @@
                                             PRIMER PROYECTO NADXIBA
 Juego de trivia, tiene tres secciones: 
 
-1ra.- Bienvenida y muestra boton para iniciar el juego. 
-2da.- Donde se  desarrolla el juego; se despliega pregunta, opciones, formulario de la respuesta y botón. 
+1ra.- Bienvenida y muestra botón para iniciar el juego. 
+2da.- Donde se desarrolla el juego; se despliega pregunta, opciones, formulario de la respuesta y botón. 
 3ra.- Pide el nombre del jugador, muestra el score final de la sesión y la lista de scores. 
 
 Se utiliza el constructor "construyePregunta" para generar las preguntas-objeto, cada una consta de: pregunta, lista
-de respuestas e indice de respuesta correcta; además se utilzan métodos de este mismo constructor para desplegar 
+de respuestas e índice de respuesta correcta; además se utilizan métodos de este mismo constructor para desplegar 
 (".prototype.imprime") y evaluar (".prototype.evalua") las preguntas en la sección correspondiente. 
 
-Se utiliza el constructor "construyeScore" para generar los score-objetos que contrendran: nombre del jugador, aciertos
+Se utiliza el constructor "construyeScore" para generar los score-objetos que contendrán: nombre del jugador, aciertos
 y total  de preguntas mostradas. 
 
 La función "final" se encarga de desplegar la información del score-objeto en la sección tres y también de  guardarlo 
-en el local storage. Por último, la función "initPregunta" se ejecuta al inicio del juego y en adelante da la función 
-"muestraPregunta" se encarga de evaluar y mostrar la siguinte pregunta (en ese orden).
+en el localStorage. Por último, la función "initPregunta" se ejecuta al inicio del juego y en adelante da la función 
+"sigPregunta" se encarga de evaluar y mostrar la siguiente pregunta (en ese orden).
 -------------------------------------------------------------------------------------------------------------------*/
 
+
+//// Constructores
 var construyePregunta = function(pregunta, respuestas, indiceCorrecto){
-	this.enunciado = pregunta;
-  	this.respuestas = respuestas;
-  	this.correcta = indiceCorrecto;
+    this.enunciado = pregunta;
+    this.respuestas = respuestas;
+    this.correcta = indiceCorrecto;
 };
 
 var construyeScore = function(correctas, totales, nombre){
-	this.correctas = correctas;
-  	this.totales = totales;
-  	this.nombre = nombre;
+    this.correctas = correctas;
+    this.totales = totales;
+    this.nombre = nombre;
 };
 
+
+//// Lista de preguntas
 var pregunta1 = new construyePregunta('¿Cuánto es 3 + 5?', [1, 3, 8, 10], 2);
 var pregunta2 = new construyePregunta('¿En qué mes estamos?', ['enero', 'febrero', 'agosto','junio'], 3);
 var pregunta3 = new construyePregunta('¿De qué color es el caballo blanco de Napoleón?', ['azul', 'blanco', 'amarillo', 'rojo'], 1);
@@ -39,26 +43,26 @@ var pregunta6 = new construyePretunta('¿Qué es Nadxiba ?', ['Empresa', 'Bar', 
 
 var preguntas = [pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, pregunta6];
 
+
+//// Métodos del prototype de construyePregunta
 construyePregunta.prototype.imprime = function(){
+    // Despliega la pregunta y las opciones en la página web
     var titulo = document.querySelector("#enunciado");
     titulo.innerHTML = this.enunciado;
     var titLi= document.getElementById("titList");
     titLi.innerHTML = "Opciones:";
-      
-    lista = this.respuestas;
-
+    var lista = this.respuestas;
     document.getElementById("listOpcs").innerHTML = '';
-
     for(i=0;i<lista.length;i++){
         var node = document.createElement("LI");
         var opcion = document.createTextNode(lista[i]);
         node.appendChild(opcion);
         document.getElementById("listOpcs").appendChild(node);
     };
-
 };
        
 construyePregunta.prototype.evalua = function(respCorrecta){
+    // Evalúa la respuesta introducida por el jugador
     n = this.correcta;
     if(respCorrecta === n){
         return ['Acertaste :D', 1];
@@ -68,20 +72,13 @@ construyePregunta.prototype.evalua = function(respCorrecta){
 };
 
 
+//// Funciones asociadas con los eventos
 var correctas = 0;
 var totales = 0;
 var selector = 0;
 
-var final = function(correctas, totales){
-	alert('Gracias por haber jugado. \n' + 'Puntaje: ' + correctas + ' aciertos de ' + totales + ' preguntas.');
-	nombre = prompt('Introduce tu nombre.');
-	window[nombre] = new construyeScore(correctas, totales, nombre);
-	localStorage.setItem(nombre, JSON.stringify(window[nombre]));
-};
-
-//Función que se ejecuta solo una vez al inciar el juego, despliega las preguntas en la página web y
-//para evaluar las respués es necesario ejecutar la función "sigPregunta"
 var initPregunta = function(){
+    // Da inicio al juego, despliega una primer pregunta en la página web y los elementos del DOM necesaarios
     selector = Math.floor(preguntas.length*Math.random());
     preguntas[selector].imprime();
     document.getElementById('init').style.display = 'none';
@@ -89,16 +86,16 @@ var initPregunta = function(){
     document.getElementById('entrada').style.display = 'block';
 };
 
-//Función principal que evalua la respuesta dada por el usuario. después despliega la siguiente pregunta
-//y sus opciones. La función se ejecuta estrictatemente en el orden explicado.
 var sigPregunta = function(){
+    // Valida y evalúa la respuesta introducida por el jugador y despliega una siguiente pregunta
+        // ESTRICTAMENTE en ese orden
     var respCorrecta = document.getElementById("respuesta").value;
     if(0<=respCorrecta && respCorrecta<4 && respCorrecta!=""){
         var n = parseInt(respCorrecta);
         var result = preguntas[selector].evalua(n)
         correctas = correctas + result[1];
         totales++;
-        console.log(result[0] + '! Llevas ' + correctas + ' correctas.');
+        console.log(result[0] + '! Llevas ' + correctas + ' correctas.'); // Esto debe hacerlo en alertas o en un apartado de la página web
         document.getElementById('respuesta').value = '';
         document.getElementById('respuesta').placeholder = 'Introduce tu respuesta';
         selector = Math.floor(preguntas.length*Math.random());
@@ -110,5 +107,18 @@ var sigPregunta = function(){
     preguntas[selector].imprime();
 }
 
+var final = function(correctas, totales){
+    // Pide el nombre al jugador y guarda su score 
+    alert('Gracias por haber jugado. \n' + 'Puntaje: ' + correctas + ' aciertos de ' + totales + ' preguntas.');
+    nombre = prompt('Introduce tu nombre.');
+    window[nombre] = new construyeScore(correctas, totales, nombre);
+    localStorage.setItem(nombre, JSON.stringify(window[nombre]));
+    // Falta que se despliegue la tabla de scores guardados
+    // Falta que muestre de nuevo el botón 'init' (o en su defecto crear uno nuevo) 
+        // y oculte el botón 'sig', para poder iniciar nuevamente el juego
+};
+
+
+//// Ligas con eventos en objetos del DOM
 document.getElementById('init').onclick = initPregunta;
 document.getElementById('sig').onclick = sigPregunta;
